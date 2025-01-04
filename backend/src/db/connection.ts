73be45d9d5fccd66197a987c842567d2cb35bf77
GautamKumarhr7 <kumargautam3231@gmail.com>
2025-01-04 12:58:20 +0530

    basice setup and connection changes

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -3,16 +3,10 @@ import{drizzle}from "drizzle-orm/node-postgres";
 import { Client } from "pg";
 dotenv.config();
 
-let connectionString="";
-if(process.env.NODE_ENV=="dev"){
-    connectionString=process.env.DATABASE_URL as string;
-    
-}
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-if (!connectionString) {
-    throw new Error("DATABASE_URL is missing!");
-}
+let connectionString ="";
+
+connectionString = process.env.DATABASE_URL!;
 export const client=new Client({
-    connectionString,
+    connectionString
 });
 export const db=drizzle(client);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,12 @@ import { client } from "./db/connection";
 import * as dotenv from "dotenv"
 import app from "./app"
 dotenv.config()
-const port:number=3000;
-async function connection(){
+// const port:number=3000;
+const port=process.env.port||3001;
+async function connectionVerifaction(){
     await client.connect()
 }
-connection().then(()=>{
+connectionVerifaction().then(()=>{
     console.log("database is connected!");
     app.listen(port,()=>{
         console.log(`server is runninnng http://localhost:${port}`)
