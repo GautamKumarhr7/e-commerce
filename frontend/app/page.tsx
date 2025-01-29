89cc33122271ef61f4a2c52b971e7bfe3a269ad6
GautamKumarhr7 <kumargautam3231@gmail.com>
2025-01-29 11:46:27 +0530

    frontend initial commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+async function home() {
+  return (
+    <div className="h-full w-full fx-centre">
+      <h1 >hii from ecommerce </h1>
+      <p>hii users i am on build</p>
+    </div>
+  );
+}
+
+export default home;
