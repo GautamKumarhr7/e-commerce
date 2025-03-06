5fa752e78da69d72657bc37e16a897b40fd97d11
Yuvraj singh <yuvrajsingh200205@gmail.com>
2025-03-07 01:09:52 +0530

    create get/post route for addToCart

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -2,12 +2,14 @@ import addProducts from "./products/addProducts";
 import getProduct from "./products/getProducts";
 import addCatogries from "./categories/addCategories";
 import getCatogries from "./categories/getcategories";
-
-
+import addItemsToCart from "./addToCart/addItemsToCart";
+import getCartItems from "./addToCart/getCartItems";
 
 export{
     addProducts,
     getProduct,
     addCatogries,
-    getCatogries
+    getCatogries,
+    addItemsToCart,
+    getCartItems
 }
